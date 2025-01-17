afe8c6e0b6d0bc387bcc9864b3accdcaf9f85621
Mark Joshwel <mark@joshwel.co>
2025-01-18 02:04:23 +0800

    meta(sota): add sota staircase ReStepper nix files

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "flake for running the sota staircase ReStepper";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs; {
+        devShells.default = mkShellNoCC {
+          buildInputs = [
+            git
+            git-lfs
+            git-filter-repo
+            (python312.withPackages (python-pkgs: [
+              python-pkgs.tqdm
+              python-pkgs.gitignore-parser
+            ]))
+          ];
+        };
+      }
+    );
+}
