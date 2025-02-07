9d4e55d640d2b5ceae924ee6c521f7fd0693e053
yauwailam <s10258533@connect.np.edu.sg>
2025-02-07 16:02:54 +0800

    simple auth on web

---
+++ Web/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyARm-eSymd2Q3AyxJXiAiiUzsXGmc6T72I",
+    authDomain: "echoesbehindcloseddoors.firebaseapp.com",
+    databaseURL: "https://echoesbehindcloseddoors-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "echoesbehindcloseddoors",
+    storageBucket: "echoesbehindcloseddoors.firebasestorage.app",
+    messagingSenderId: "905742343227",
+    appId: "1:905742343227:web:19d04f77371ef1952c901e"
+  };
