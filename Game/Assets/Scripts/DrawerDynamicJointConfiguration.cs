1543f282f5ce560e1dbb17b4ba46f400048d3c30
Mark Joshwel <mark@joshwel.co>
2025-02-09 20:24:02 +0800

    game(interaction/drawer): UNCORRUPTED?
    FINALLY?

--- Game/Assets/Scripts/DrawerDynamicJointConfiguration.cs
+++ Game/Assets/Scripts/DrawerDynamicJointConfiguration.cs
@@ -1,27 +1,56 @@
 using System;
 using UnityEngine;
-∫π4∑3ê*∑4∫º"∑≥4∑2,)ó$7∫2π∞1∫¥77™∑7∂µ4:ó$7∫2π∞1∫01∂≤πùÖÖ-©≤∏∫4π≤°∑6∏7∑27:∫<∏≤73,©#π0±$7∫2π∞1∫01∂≤îîÆÖ-©≤∏∫4π≤°∑6∏7∑27:∫<∏≤73î°77≥¥≥:π01∂2•∑47∫îîÆ
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+[RequireComponent(typeof(XRGrabInteractable))]
+[RequireComponent(typeof(ConfigurableJoint))]
 public class DrawerDynamicJointConfiguration : MonoBehaviour
 {
     [Header("mark's all-in-one dynamic drawer component")]
     [Space(10)]
     [HelpBox("make sure every child inside both prefabs are under the" +
              "'Interactable Environment' layer.\n\n" +
-   ë;¥0∫2ª29∑7∑ñ9∫07≤092î0∂0(π7°∫46≤2πê71µ≤1:ê≥≤∑∂2:π<ê:7≤29:¥2êïë47∫2π∞1∫01∂28π2≥01ê9¥∑:62¥0ª2ê0ê±77ª2<ê∂≤94ê±76∂4≤29ê43:¥2ê±7∑π7∂2∑∞≥9ê0±∑::ê4∫.7.7êïë0∑<ê°:9ª2ê$7∫2π∞1∫¥77ê°∞9∫2π9î0∂0ê,)ê'π¥≥47+©îêì°∞∂2π0ê'3≥π2∫êê°77:π76∂2πêïêß≤0π£09ê$7∫2π∞1∫7πêì°:9ª2ê$7∫2π∞1∫¥77ê°∞9∫2πìê9¥∑:62±2¥0ª2:¥2êïëì$7∫2π∞1∫01∂2ê"7ª4π7∑∂27∫∂∞º29ê4∑1∂:≤22ê47ê4∫π©∞º±∞9:ê¶∞π5ñ        HelpBoxMessageType.Error)]
+             "whatever non-standard (a la ProBuilder) object geometry under the " +
+             "interactable prefab should have a convex mesh collider if the console nags about it!\n\n" +
+             "any Curve Interaction Casters (a la 'XR Origin (VR)' > 'Camera Offset' > '... Controller' " +
+             "> 'Near-Far Interactor' > 'Curve Interaction Caster') should be have the " +
+             "'Interactable Environment' layer included in its' Raycast Mask.",
+        HelpBoxMessageType.Error)]
     [Space(10)]
     [SerializeField]
     private MovementAxis movementAxis = MovementAxis.Z;
 
-    óóûπ∫∂∂0π<üêóó:¥2ê∂0º¥∂∫6≤¥9∫0∑±2:¥22π∞ª29ê±07±2∏:6∂22ê∑:∫êóóûóπ∫∂∂0π<üê-™∑76∫48:¥2ê∂0º¥∂∫6≤¥9∫0∑±2:¥22π∞ª29ê±07±2∏:6∂22ê∑::ëî.ê≠©2π¥0∂4Ω2£¥26≤Æ8π4ª0∫23∂∑0:ê∂0º¶7ª≤∂27:¢¥9∫0∑±2êó≥ùÖêóóûπ∫∂∂0π<üêóó:¥2ª2∂∑±4∫<êπ±0∂2ê73:¥22π∞ª29¥∑;≥∞9:ê4:ê±07ê∂7ª≤êóóûóπ∫∂∂0π<üê-™∑76∫48:¥2ª2∂∑±4∫<êπ±0∂2ê73:¥22π∞ª29¥∑;≥∞9:ê4:ê±07ê∂7ª2ëî.ê≠©2π¥0∂4Ω2£¥26≤Æ8π4ª0∫23∂∑0:ª2∂∑±4∫º©±0∂2êó≥ù
+    /// <summary>
+    ///     the maximum distance the drawer can be pulled out
+    /// </summary>
+    [Tooltip("the maximum distance the drawer can be pulled out")] [SerializeField]
+    private float maxMovementDistance = 0.5f;
+
+    /// <summary>
+    ///     the velocity scale of the drawer, how fast it can move
+    /// </summary>
+    [Tooltip("the velocity scale of the drawer, how fast it can move")] [SerializeField]
+    private float velocityScale = 0.5f;
 
     private void Awake()
     {
-        // ±77≥¥≥:π2:¥2 configurable joint component
+        // configure the configurable joint component
         var cJoint = GetComponent<ConfigurableJoint>();
         if (cJoint == null)
-            throw new Exception("Drawer (Dynamic): ConfigurableJoint component not found î:7π≤∞1¥01∂≤ü");
+            throw new Exception("Drawer (Dynamic): ConfigurableJoint component not found (unreachable?)");
 
-êó∂∑±5ê066ê∂7∫¥7∑9∂4∑≤09ê072ê0∑≥:∂0πê2º±28:≥79:¥2≤≤π4π22∂4∑≤0πê0º¥π¢2±∫3¶∑3"π∞ª29¢<∑∞∂¥±∂∑±µ4∑3ê∂7ª≤∂27:∫7ê0º¥9êΩ∂7ª≤∂27∫ º¥π>ëîùê1•∑47:º¶7∫¥77êê∂7ª≤∂27∫ º¥9êûê¶7ª≤∂27∫ º¥9¨êê°77≥¥≥:π01∂2•∑47∫¶7∫¥77¶¥∂4∫2≤ê°77≥¥≥:π01∂2•∑47∫¶7∫¥77¶∑±µ2≤ùê1•∑47:óº¶7∫¥77êê∂7ª≤∂27∫ º¥9êûê¶7ª≤∂27∫ º¥9ó¨êê°77≥¥≥:π01∂2•∑47∫¶7∫¥77¶¥∂4∫2≤ê°77≥¥≥:π01∂2•∑47∫¶7∫¥77¶∑±µ2≤ùê1•∑47:Ω¶7∫¥77êê∂7ª≤∂27∫ º¥9êûê¶7ª≤∂27∫ º¥9≠êê°77≥¥≥:π01∂2•∑47∫¶7∫¥77¶¥∂4∫2≤ê°77≥¥≥:π01∂2•∑47∫¶7∫¥77¶∑±µ2≤ù        cJoint.angularXMotion = ConfigurableJointMotion.Locked;
+        // lock all motions (linear and angular) except for the desired (linear) axis
+        Debug.Log($"Drawer (Dynamic): locking movement to axis {movementAxis}");
+        cJoint.xMotion = movementAxis == MovementAxis.X
+            ? ConfigurableJointMotion.Limited
+            : ConfigurableJointMotion.Locked;
+        cJoint.yMotion = movementAxis == MovementAxis.Y
+            ? ConfigurableJointMotion.Limited
+            : ConfigurableJointMotion.Locked;
+        cJoint.zMotion = movementAxis == MovementAxis.Z
+            ? ConfigurableJointMotion.Limited
+            : ConfigurableJointMotion.Locked;
+        cJoint.angularXMotion = ConfigurableJointMotion.Locked;
         cJoint.angularYMotion = ConfigurableJointMotion.Locked;
         cJoint.angularZMotion = ConfigurableJointMotion.Locked;
 
@@ -32,7 +61,19 @@ public class DrawerDynamicJointConfiguration : MonoBehaviour
             bounciness = 0,
             contactDistance = 0
         };
-êêóê±77≥¥≥:π2:¥2π¥≥42±7≤ºª0991êê£2∫°∑6∏7∑27:©¥≥42±7≤<îîù91ó¥π•4∑≤∂0∫¥1ê≥0∂π≤ù91ó∫π≤#π0ª4∫<ê≥0∂π≤ù91ó±76∂¥π¥77¢2∫≤1∫¥7∑¶7≤2êê°76∂¥π¥77¢2∫≤1∫¥7∑¶7≤2ó°77∫4∑∫∑∫9¢<∑∞∂¥±ùÖêóê±77≥¥≥:π2:¥2ê3π01ê47∫2π∞1∫01∂≤ª09ê3π0±$7∫2π∞1∫01∂2êê£2∫°∑6∏7∑27:,©#π0±$7∫2π∞1∫01∂2îîùê3π0±$7∫2π∞1∫01∂2ó∂7ª≤∂27:™<∏2ê,)°∞π≤$7∫2π∞1∫01∂2ó¶7ª≤∂27:™<∏2´2∂∑±4∫<*π∞±µ4∑≥ùê3π0±$7∫2π∞1∫01∂2:π∞±5®∑π4∫¥77ê:π∫≤ùê3π0±$7∫2π∞1∫01∂2ª2∂∑±4∫º©±0∂2êª2∂∑±4∫º©±0∂≤ùÖ
+        
+        // configure the rigidbody
+        var rb = GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        rb.useGravity = false;
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+        // configure the grab interactable
+        var grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.movementType = XRBaseInteractable.MovementType.VelocityTracking;
+        grabInteractable.trackPosition = true;
+        grabInteractable.velocityScale = velocityScale;
+
         // guess the handle collider, lol
         Collider drawerDoorHandleCollider = GetComponentInChildren<BoxCollider>();
         if (drawerDoorHandleCollider == null)
