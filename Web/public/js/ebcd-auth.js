7c373b7a8a62d2e58df340bc242928eaaaca4251
Mark Joshwel <mark@joshwel.co>
2025-02-13 16:08:29 +0800

    web: SSO WORKS

--- Web/public/js/ebcd-auth.js
+++ Web/public/js/ebcd-auth.js
@@ -1,4 +1,9 @@
-import { auth } from "./ebcd-common-firebase.js";
+import {
+  auth,
+  authProviderGoogle,
+  authWithMagicLink,
+  authWithGooglePopup,
+} from "./ebcd-common-firebase.js";
 
 console.log("ebcd-auth loaded <3");
 
@@ -6,4 +11,104 @@ auth.onAuthStateChanged((user) => {
   if (user) {
     window.location = "/profile.html";
   }
+});
+
+const actionCodeSettings = {
+  url: `${window.location.origin}/profile.html`,
+  handleCodeInApp: true,
+  iOS: {
+    bundleId: "co.wirm.ebcd",
+  },
+  android: {
+    packageName: "co.wirm.ebcd",
+    installApp: true,
+    minimumVersion: "12",
+  },
+  dynamicLinkDomain: "echoesbehindcloseddoors.page.link",
+};
+
+// input.. id="auth-field-email"
+// a...... id="auth-button-magic"
+// a...... id="auth-button-sso-google"
+// h2..... id="auth-modal-result-header"
+// p...... id="auth-modal-result-body"
+
+// handle magic link auth
+const buttonSendLink = document.getElementById("auth-button-magic");
+buttonSendLink.addEventListener("click", async (event) => {
+  console.log("ebcd-auth: clicked magic");
+
+  const modalResultHeader = document.getElementById("auth-modal-result-header");
+  const modalResultBody = document.getElementById("auth-modal-result-body");
+  const fieldEmail = document.getElementById("auth-field-email");
+  // try get email from fieldEmail.value
+  const email = fieldEmail ? fieldEmail.value.trim() : null;
+
+  // if email is blank
+  if (!email) {
+    modalResultHeader.innerText = "Error!";
+    modalResultBody.innerText = "Please enter an email address.";
+    return;
+  }
+
+  // if email is not valid
+  if (!email.includes("@")) {
+    modalResultHeader.innerText = "Error!";
+    modalResultBody.innerText = "Please enter a valid email address.";
+    return;
+  }
+
+  console.log("ebcd-auth/auth-button-magic: sent!");
+  authWithMagicLink(auth, email, actionCodeSettings)
+    .then(() => {
+      console.log("ebcd-auth/auth-button-magic: success!");
+      // The link was successfully sent. Inform the user.
+      modalResultHeader.innerText = "You've Got Mail!";
+      modalResultBody.innerText = `We've sent you an email to ${email} with a magical link to authenticate with us. If you're a new user, you'll make an account. If you're an existing user, you'll be logged in. Check your inbox!`;
+
+      // Save the email locally so you don't need to ask the user for it again
+      // if they open the link on the same device.
+      window.localStorage.setItem("emailForSignIn", email);
+    })
+    .catch((error) => {
+      console.log(
+        "ebcd-auth/auth-button-magic: error...",
+        error.code,
+        error.message
+      );
+      modalResultHeader.innerText = "Error!";
+      modalResultBody.innerHTML = `We've encountered an error. Tell the booth runner about this!<br>${error.message}`;
+    });
+});
+
+// handle sso auth
+// <button id="button-sso-google">
+const buttonSSOGoogle = document.getElementById("auth-button-sso-google");
+buttonSSOGoogle.addEventListener("click", async (event) => {
+  console.log("ebcd-auth: clicked sso");
+
+  const provider = new authProviderGoogle();
+  const modalResultHeader = document.getElementById("auth-modal-result-header");
+  const modalResultBody = document.getElementById("auth-modal-result-body");
+
+  console.log("ebcd-auth/auth-button-sso-google: sent!");
+  provider.setCustomParameters({
+    prompt: "select_account", // forces account selection every time
+  });
+
+  authWithGooglePopup(auth, provider)
+    .then(() => {
+      // redirect to profile page
+      window.location = "/profile.html";
+    })
+    .catch((error) => {
+      // show error
+      console.log(
+        "ebcd-auth/auth-button-sso-google: error...",
+        error.code,
+        error.message
+      );
+      modalResultHeader.innerText = "Error!";
+      modalResultBody.innerHTML = `We've encountered an error. Tell the booth runner about this!<br>${error.message}`;
+    });
 });
--- Web/public/js/ebcd-common-firebase.js
+++ Web/public/js/ebcd-common-firebase.js
@@ -1,17 +1,28 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-database.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js";
+import {
+  getAuth,
+  sendSignInLinkToEmail,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut as _signOut,
+} from "https://www.gstatic.com/firebasejs/11.3.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyARm-eSymd2Q3AyxJXiAiiUzsXGmc6T72I",
   authDomain: "echoesbehindcloseddoors.firebaseapp.com",
-  databaseURL: "https://echoesbehindcloseddoors-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://echoesbehindcloseddoors-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "echoesbehindcloseddoors",
   storageBucket: "echoesbehindcloseddoors.firebasestorage.app",
   messagingSenderId: "905742343227",
-  appId: "1:905742343227:web:19d04f77371ef1952c901e"
+  appId: "1:905742343227:web:19d04f77371ef1952c901e",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const signOut = _signOut;
+export const authWithMagicLink = sendSignInLinkToEmail;
+export const authWithGooglePopup = signInWithPopup;
+export const authProviderGoogle = GoogleAuthProvider;
 export const database = getDatabase(app);
