fea0141ce17e0f1e3a8195e02a9d0f75a1e86cf4
Mark Joshwel <mark@joshwel.co>
2025-02-14 22:20:14 +0800

    game(scripts): standardise EffectTrigger

--- Game/Assets/Scripts/EffectTrigger.cs
+++ Game/Assets/Scripts/EffectTrigger.cs
@@ -1,5 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+/*
+ * Author: Reza
+ * Date: 13/2/2025
+ * Description: Car obstacle
+ */
+
 using UnityEngine;
 
 public class EffectTrigger : MonoBehaviour
@@ -7,26 +11,21 @@ public class EffectTrigger : MonoBehaviour
     // The effect name to trigger when the player enters this area
     public string effectName;
 
-    // Check if the player enters the trigger zone
-    private void OnTriggerStay(Collider other)
-    {
-        // Check if the player is in the trigger zone
-        if (other.CompareTag("Player"))
-        {
-            // Trigger the effect based on the specified effectName
-            if (!PostProcessingManager.Instance.IsEffectActive())
-            {
-                PostProcessingManager.Instance.TriggerEffect(effectName);
-            }
-        }
-    }
-
     // Optionally stop the effect when the player exits the trigger zone
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player") && PostProcessingManager.Instance.IsEffectActive())
-        {
             PostProcessingManager.Instance.StopEffect();
-        }
+    }
+
+    // Check if the player enters the trigger zone
+    private void OnTriggerStay(Collider other)
+    {
+        // Check if the player is in the trigger zone
+        if (!other.CompareTag("Player")) return;
+
+        // Trigger the effect based on the specified effectName
+        if (!PostProcessingManager.Instance.IsEffectActive())
+            PostProcessingManager.Instance.TriggerEffect(effectName);
     }
 }
