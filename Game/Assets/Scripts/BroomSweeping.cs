c424f6fdd6c4f0ec88f7587f11e391d3b2712e46
Mark Joshwel <mark@joshwel.co>
2025-02-14 21:57:22 +0800

    game(scripts): standardise BroomSweeping

--- Game/Assets/Scripts/BroomSweeping.cs
+++ Game/Assets/Scripts/BroomSweeping.cs
@@ -1,56 +1,54 @@
 /*
-Author: Reza
-Date: 7/2/25
-Description: Detects dirt and sweeps them up 
-*/
+ * Author: Reza
+ * Date: 7/2/25
+ * Description: Detects dirt and sweeps them up
+ */
 
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class BroomSweeping : MonoBehaviour
 {
-    private GameManager gameManager;
-    private PostProcessingManager PostProcessingManager;
-    
     // To track how much trash has been collected so far
-    public int dirtSweeped = 0;
-    
+    public int dirtSweeped;
+
     // Defines how much trash is needed to collect in order to unlock the door
     public int dirtRequired = 10;
-    
-    private bool taskCompleted = false;
-    
+
     // Defines UI references
-    [Header("UI References")]
-    public GameObject storyPanelUI;
+    [Header("UI References")] public GameObject storyPanelUI;
+
     public TMP_Text storyText;
-    
+
     // Defines Audio References
-    [Header("Audio References")]
-    public AudioSource audioSource;
+    [Header("Audio References")] public AudioSource audioSource;
+
     public AudioClip sweepingSound;
-    
+    private GameManager gameManager;
+    private PostProcessingManager PostProcessingManager;
+
+    private bool taskCompleted;
+
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (dirtSweeped >= dirtRequired && !taskCompleted)
         {
-            taskCompleted = true; 
+            taskCompleted = true;
             GameManager.Instance.FloorSweepedTaskComplete();
-            
+
             storyPanelUI.SetActive(true);
             storyText.text = "I hope the house is clean enough now so I don't get scolded later...";
-            
+
             StartCoroutine(ClearMessageAfterSeconds(7f));
         }
     }
-    
+
     private void OnTriggerEnter(Collider other)
     {
         // Now correctly checks for "Dirt" before triggering
-        if (other.CompareTag("Dirt"))  
+        if (other.CompareTag("Dirt"))
         {
             dirtSweeped++;
 
@@ -58,13 +56,10 @@ public class BroomSweeping : MonoBehaviour
             Destroy(other.gameObject);
 
             // Play sound only if no other sound is currently playing
-            if (!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(sweepingSound);
-            }
+            if (!audioSource.isPlaying) audioSource.PlayOneShot(sweepingSound);
         }
     }
-    
+
     private IEnumerator ClearMessageAfterSeconds(float delay)
     {
         // Waits for delay to end and hides the UI
